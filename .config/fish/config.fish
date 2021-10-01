45098de49c37c0891c414864e6b707dd7051cf2a
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2021-10-01 18:16:41 +0100

    New semester makeover

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    cat ~/.cache/wal/sequences
 end
+
+starship init fish | source
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/ruir/.ghcup/bin $PATH # ghcup-env
