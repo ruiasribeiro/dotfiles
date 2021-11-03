f1fa7741736856af5f29102a9e143e43550f75de
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2021-11-03 20:34:06 +0000

    Small improvements

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    cat ~/.cache/wal/sequences
+    
+    # Activates pywal color scheme
+    # cat ~/.cache/wal/sequences
 end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/ruir/.ghcup/bin $PATH # ghcup-env
@@ -11,5 +13,5 @@ set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; se
 # <<< conda initialize <<<
 
 starship init fish | source
-
-source ~/.cache/wal/colors.fish
+fish_ssh_agent
+# source ~/.cache/wal/colors.fish
