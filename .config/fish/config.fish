69f87df6fccd9d56e4a17822667d72facb402fd0
Rui Ribeiro <46627071+raddrax@users.noreply.github.com>
2022-02-24 22:56:33 +0000

    Misc changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,21 +11,8 @@ end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/ruir/.ghcup/bin $PATH # ghcup-env
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# eval /usr/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
 starship init fish | source
 # fish_ssh_agent
 # source ~/.cache/wal/colors.fish
 
 set -gx JAVA_HOME /etc/alternatives/jre
-
-# set theme
-# theme.sh ayu-mirage-simple-cursor
-
-# opam configuration
-source /home/ruir/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-
-zoxide init fish | source
