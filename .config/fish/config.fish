82396383a20168d371ea6130491e692663759c34
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com>
2024-03-01 09:25:16 +0000

    Clear setup

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here.
-
-    # Start the Starship prompt.
-    starship init fish | source
-end
-
-# Dotfiles helper.
-alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
--- README.md
+++
@@ -1,7 +0,0 @@
-# dotfiles
-My personal dotfiles.
-
-This repository was set up based on [this tutorial](https://www.atlassian.com/git/tutorials/dotfiles).
-
-![Current setup](current_setup.png)
-
