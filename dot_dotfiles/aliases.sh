8b6b32d67a601440acb5e85d52657b8d29f22482
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com>
2025-04-29 10:32:36 +0100

    update dotfiles

--- dot_dotfiles/aliases.sh
+++ dot_dotfiles/aliases.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-alias redot='. $HOME/.bashrc'
+alias reload='. $HOME/.bashrc'
 
 alias cm='chezmoi'
 alias cmra='chezmoi re-add'
 alias cmup='chezmoi git add . && chezmoi git -- commit -m "update dotfiles" && chezmoi git push'
+
+alias lls='LC_COLLATE="C" ls -hal --group-directories-first'
 
 alias rcd='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
