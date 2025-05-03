4c3d68f1b06a793ae1ac782e5360f799ff6ffc2d
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com>
2025-05-03 19:00:34 +0100

    update dotfiles

--- dot_dotfiles/aliases.sh
+++ dot_dotfiles/aliases.sh
@@ -6,6 +6,12 @@ alias cm='chezmoi'
 alias cmra='chezmoi re-add'
 alias cmup='chezmoi git add . && chezmoi git -- commit -m "update dotfiles" && chezmoi git push'
 
+# Delete all local branches (other than the primary ones). Other branches can
+# also be ignored, if added to the `grep` expression.
+alias git_clean_local='git branch | grep -v -E "main|master|dev|develop" | xargs git branch -D'
+# Prune local tracking branches not on remote.
+alias git_prune_old='git remote prune origin'
+
 alias lls='LC_COLLATE="C" ls -hal --group-directories-first'
 
 alias rcd='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
