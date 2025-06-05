9fbb10c7dd92900eb522bd043d9fa39c7d40d6ea
Rui Ribeiro <46627071+ruiasribeiro@users.noreply.github.com.>
2025-06-05 20:21:15 +0100

    update dotfiles

--- dot_dotfiles/initialisation/fzf.bash
+++
@@ -1,7 +0,0 @@
-fzf_version="$(fzf --version | awk '{print $1}')"
-
-if dpkg --compare-versions "$fzf_version" lt 0.48.0; then
-    source /usr/share/doc/fzf/examples/key-bindings.bash
-else
-    eval "$(fzf --bash)"
-fi
